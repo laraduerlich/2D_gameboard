ea3e421ba106550965a0b880a58c6caeeba5e97c
laraduerlich <lara.duerlich@t-online.com>
2024-11-21 11:25:33 +0100

    add PlayerCharacter Test and method

--- src/test/java/PlayerCharacterTest.java
+++ src/test/java/PlayerCharacterTest.java
@@ -3,16 +3,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
 public class PlayerCharacterTest {
-/*    @Test
-    void move_expectUp_GivenW() {
-        // GIVEN
-        char up = 'w';
-        // WHEN
-        getX
-        // THEN
-    }
 
-*/
 @Test
 void getX_expect0_whenStart() {
     // GIVEN
@@ -22,4 +13,23 @@ void getX_expect0_whenStart() {
     // THEN
     Assertions.assertEquals(startX, startActual);
 }
+
+@Test
+    void getY_expect0_whenStart() {
+        // GIVEN
+        int startY = 0;
+        // WHEN
+        int startActual = PlayerCharacter.getY();
+        // THEN
+        Assertions.assertEquals(startY, startActual);
+    }
+
+    @Test
+    void move_expectUp_GivenW() {
+        // GIVEN
+        char up = 'w';
+        // WHEN
+
+        // THEN
+    }
 }
